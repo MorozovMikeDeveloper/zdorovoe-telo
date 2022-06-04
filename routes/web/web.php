e1beebca30265ae0849da9473b56f7b02f840eeb
MorozovMike <morozov.mishka@gmail.com>
2022-06-04 22:13:20 +0300

    Баг фикс Не открывается страница из письма

--- routes/web/web.php
+++ routes/web/web.php
@@ -98,7 +98,7 @@ Route::post('/forgot-password', function (Request $request) {
 })->middleware('guest')->name('password.email');
 
 Route::get('/reset-password/{token}', function ($token) {
-    return view('auth.reset-password', ['token' => $token]);
+    return view('auth.passwords.reset', ['token' => $token]);
 })->middleware('guest')->name('password.reset');
 
 Route::post('/reset-password', function (Request $request) {
